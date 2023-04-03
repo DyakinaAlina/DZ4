a83b148abd22c6b0b6076e5b8e1b728acce190eb
alinadyakina <alinadyakina@5A8B>
2023-04-03 18:50:52 +0300

    Добавьте файлы проекта.

---
+++ DZ_4/DataBase.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DZ_4
+{
+    public class DataBase : IDisposable
+    {
+        string _connectionString = (@"Data Source = 5A8B\SQLEXPRESS; Initial Catalog = Alina1; Integrated Security = True");
+        SqlConnection _connection;
+
+        public DataBase()
+        {
+            _connection = new SqlConnection(_connectionString);
+            OpenConnection();
+        }
+
+        public void OpenConnection()
+        {
+            _connection.Open();
+        }
+
+        public void CloseConnection()
+        {
+            _connection.Close();
+        }
+
+        public DataTable ExecuteSql(string sql)
+        {
+            DataTable dt = new DataTable();
+            var reader = new SqlCommand(sql, _connection).ExecuteReader();
+            dt.Load(reader);
+            return dt;
+        }
+
+        public void ExecuteSqlNonQuery(string sql)
+        {
+            new SqlCommand(sql, _connection).ExecuteNonQuery();
+        }
+
+        public void Dispose()
+        {
+            CloseConnection();
+        }
+
+    }
+}
---
+++ DZ_4/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace DZ_4
+{
+    public partial class Form1 : Form 
+    {
+        public int count = 0;
+        DataTable Persons;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+
+            string login = textBoxLogin.Text;
+            string password = textBoxPassword.Text;
+
+            using (DataBase db = new DataBase())
+            {
+                Persons = db.ExecuteSql($"select * from Persons where UserLogin = '{login}' and UserPassword = '{password}'");
+
+                var data = Persons.Rows[0].ItemArray;
+                if (Persons.Rows.Count > 0)
+                {
+                    FormAdmin fa = new FormAdmin();
+                    if (data[6].ToString() == "1")
+                    {
+                        
+                        fa.BackColor = Color.Red;
+                        
+                        //this.OpenNewForm(new FormAdmin(), true);
+                    }
+                    else
+                    {
+                        fa.BackColor = Color.Green;
+                    }
+                    fa.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Неправильный логин или пароль!");
+                }
+            }
+        }
+
+        private void FormAdmin(FormAdmin formAdmin, bool v)
+        {
+            formAdmin.Show();
+            //this.OpenNewForm(new FormAdmin(), true);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+    }
+
